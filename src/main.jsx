67c0a7bf313fc55b13abea8f8b4b642651491a08
FatmaHossam5 <eng.fatma.fateh@gmail.com>
2025-04-21 09:25:44 +0200

    <style>(main):remove empty lines

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,7 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.bundle.js";
-// import "./App.css";
-// import AuthContextProvider from "./Components/Helpers/Context/AuthContext.jsx";
-// import ModalContextProvider from "./Components/Helpers/Context/ModalContext.jsx";
 
-// import ToastContextProvider from "./Components/Helpers/Context/ToastContext .jsx";
-
-// import './i18n.js'; // Import i18n configuration
-
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <AuthContextProvider>
-//       <ModalContextProvider>
-//         <ToastContextProvider>
-//           <App />
-//         </ToastContextProvider>
-//       </ModalContextProvider>
-//     </AuthContextProvider>
-//   </React.StrictMode>
-// );
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/css/bootstrap.rtl.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import './i18n.js'; // Import i18n configuration
 import ToastContextProvider from "./Components/Helpers/Context/ToastContext .jsx";
@@ -38,7 +12,7 @@ import { TabsProvider } from "./Components/Helpers/Context/TabContext.jsx";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
+  
     <AuthContextProvider>
       <ModalContextProvider>
         <ToastContextProvider>
@@ -48,5 +22,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         </ToastContextProvider>
       </ModalContextProvider>
     </AuthContextProvider>
-  // </React.StrictMode>
+
 );
